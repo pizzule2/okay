d6c1713acd8312b1f0d2fc7f953e04ea289c26eb
unknown <akintayopeter455@gmil.com>
2024-01-16 02:51:05 -0800

    commit

---
+++ external.js
@@ -0,0 +1,6 @@
+function peter(){
+    document.querySelector('h1').innerHTML='white';
+    document.querySelector('h1').innerHTML='it is javascript class';
+    document.querySelector('img').src='airoplan.jpeg';
+    document.querySelector('body').style.background='blue'
+}
